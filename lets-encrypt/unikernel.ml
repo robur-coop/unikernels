0f92293e09fe6152d0f6e91f17b72d4782cc99bd
Hannes Mehnert <hannes@mehnert.org>
2018-11-14 20:16:54 +0100

    respect production flag

--- lets-encrypt/unikernel.ml
+++ lets-encrypt/unikernel.ml
@@ -215,7 +215,13 @@ module Client (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) (R
     let account_key = gen_rsa (Key_gen.account_key_seed ()) in
     Conduit_mirage.with_tls ctx >>= fun ctx ->
     let ctx = Cohttp_mirage.Client.ctx res ctx in
-    Acme.initialise ~ctx ~directory:Letsencrypt.letsencrypt_url account_key >>= function
+    let directory =
+      if Key_gen.production () then
+        Letsencrypt.letsencrypt_url
+      else
+        Letsencrypt.letsencrypt_staging_url
+    in
+    Acme.initialise ~ctx ~directory account_key >>= function
     | Error e -> Logs.err (fun m -> m "error %s" e) ; Lwt.return_unit
     | Ok le ->
       Logs.info (fun m -> m "initialised lets encrypt") ;
