285e6a2ddabd473e593be08e324a3eb348fe2a92
Hannes Mehnert <hannes@mehnert.org>
2019-03-03 20:01:39 +0100

    retreat configuration

--- resolver/unikernel.ml
+++ resolver/unikernel.ml
@@ -4,6 +4,54 @@ open Lwt.Infix
 
 open Mirage_types_lwt
 
+let data =
+  let n = Domain_name.of_string_exn
+  and ip = Ipaddr.V4.of_string_exn
+  and s = Domain_name.Set.singleton
+  in
+  let domain = n "mirage" in
+  let m = Domain_name.prepend_exn domain in
+  let ns = m "ns"
+  and ttl = 2560l
+  in
+  let ip_set i = Dns_map.Ipv4Set.singleton i in
+  let ptr_zone = n "42.168.192.in-addr.arpa" in
+  let ptr_name = Domain_name.prepend_exn ptr_zone in
+  let records = [
+    (m "router", ip "192.168.42.1", ptr_name "1") ;
+    (m "dhcp", ip "192.168.42.2", ptr_name "2") ;
+    (ns, ip "192.168.42.3", ptr_name "3") ;
+    (m "opam", ip "192.168.42.5", ptr_name "5") ;
+    (m "pad", ip "192.168.42.6", ptr_name "6") ;
+    (m "monitor", ip "192.168.42.7", ptr_name "7") ;
+    (domain, ip "192.168.42.8", ptr_name "8") ;
+  ] in
+  let soa = Dns_packet.({ nameserver = ns ;
+                          hostmaster = m "hostmaster" ;
+                          serial = 1l ; refresh = 10l ; retry = 5l ;
+                          expiry = 600l ; minimum = ttl })
+  in
+  let open Dns_map in
+  let open Dns_trie in
+  let t = insert domain Soa (ttl, soa) empty in
+  let t = insert domain Ns (ttl, s ns) t in
+  let t =
+    List.fold_left
+      (fun trie (name, ip, _) -> insert name A (ttl, ip_set ip) trie)
+      t records
+  in
+  let ptr_soa = Dns_packet.({ nameserver = ns ;
+                              hostmaster = n "hostmaster.example" ;
+                              serial = 42l ; refresh = 16384l ; retry = 2048l ;
+                              expiry = 1048576l ; minimum = ttl })
+  in
+  let t = insert ptr_zone Soa (ttl, ptr_soa) t in
+  let t = insert ptr_zone Ns (ttl, s ns) t in
+  let t = insert (ptr_name "4") Ptr (ttl, n "cal.robur.io") t in
+  List.fold_left
+    (fun trie (name, _, ptr) -> insert ptr Ptr (ttl, name) trie)
+    t records
+
 module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) = struct
   module D = Dns_mirage_resolver.Make(R)(P)(M)(T)(S)
 
@@ -11,11 +59,13 @@ module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) = st
     let trie =
       List.fold_left
         (fun trie (k, v) -> Dns_trie.insertb k v trie)
-        Dns_trie.empty Dns_resolver_root.reserved_zones
+        data Dns_resolver_root.reserved_zones
     in
     let keys = [
       Domain_name.of_string_exn ~hostname:false "foo._key-management" ,
-      { Dns_packet.flags = 0 ; key_algorithm = Dns_enum.SHA256 ; key = Cstruct.of_string "/NzgCgIc4yKa7nZvWmODrHMbU+xpMeGiDLkZJGD/Evo=" }
+      { Dns_packet.flags = 0 ; key_algorithm = Dns_enum.SHA256 ; key = Cstruct.of_string "/NzgCgIc4yKa7nZvWmODrHMbU+xpMeGiDLkZJGD/Evo=" } ;
+      (Domain_name.of_string_exn ~hostname:false "dhcp._update",
+       { Dns_packet.flags = 0 ; key_algorithm = Dns_enum.SHA256 ; key = Cstruct.of_string "1W1QTpMwpNqnikpYztxjaKmltDpsmvWW70+itztu82g=" })
     ] in
     (match Dns_trie.check trie with
      | Ok () -> ()
