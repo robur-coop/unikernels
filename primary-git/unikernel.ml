00afd873cf53352e50a5b8435542bb8bda02fa29
Hannes Mehnert <hannes@mehnert.org>
2019-03-21 15:08:43 +0100

    primary-git: actually push and pull!

--- primary-git/unikernel.ml
+++ primary-git/unikernel.ml
@@ -32,9 +32,30 @@ module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) (RES
               | Ok data -> (name, data) :: acc)
           [] files
 
+  let load_git conduit resolver =
+    load_zones conduit resolver >|= fun bindings ->
+    Logs.info (fun m -> m "found %d bindings: %a" (List.length bindings)
+                  Fmt.(list ~sep:(unit ",@ ") (pair ~sep:(unit ": ") string int))
+                  (List.map (fun (k, v) -> k, String.length v) bindings)) ;
+    List.fold_left (fun trie (k, data) ->
+        match Udns_zonefile.load [] data with
+        | Error msg ->
+          Logs.err (fun m -> m "error while loading zonefile %s: %s" k msg) ;
+          trie
+        | Ok rrs ->
+          let trie' = Udns_trie.insert_map (Udns_map.of_rrs rrs) trie in
+          match Udns_trie.check trie' with
+          | Ok () -> trie'
+          | Error e ->
+            Logs.err (fun m -> m "error (while processing %s) %a during check()"
+                         k Udns_trie.pp_err e) ;
+            trie)
+      Udns_trie.empty bindings
+
   module Store = Irmin_mirage.Git.KV_RW(Irmin_git.Mem)(P)
 
   let store_zone t zone =
+    (* TODO maybe make this conditionally on modifications of the zone? *)
     match Udns_server.text zone (Udns_server.Primary.server t) with
     | Error msg ->
       Logs.err (fun m -> m "error while converting zone %a: %s" Domain_name.pp zone msg) ;
@@ -50,19 +71,22 @@ module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) (RES
     with
     | Error e ->
       Logs.err (fun m -> m "error while folding while storing zones %a" Udns_trie.pp_e e) ;
-      assert false
+      Lwt.return_unit
     | Ok actions ->
       Irmin_git.Mem.v (Fpath.v ".") >>= function
       | Error _ -> assert false
       | Ok git ->
         Store.connect git ~conduit ~resolver ~author:"primary DNS"
           ~msg:(fun _ -> "a zone change") () (Key_gen.remote ()) >>= fun store ->
+        (* TODO removal of a zone should lead to dropping this zone from git! *)
         Store.batch store (fun s ->
             Lwt_list.fold_left_s
               (fun r f -> match r with Ok () -> f s | Error e -> Lwt.return (Error e))
               (Ok ()) actions) >|= function
         | Ok () -> Logs.app (fun m -> m "pushed zonefiles")
-        | Error e -> Logs.err (fun m -> m "error while pushing zonefiles %a" Store.pp_write_error e)
+        | Error e ->
+          (* TODO bail out!? try again? (https://github.com/mirage/ocaml-git/issues/342) *)
+          Logs.err (fun m -> m "error while pushing zonefiles %a" Store.pp_write_error e)
 
   module D = Udns_mirage_server.Make(P)(M)(T)(S)
 
@@ -73,31 +97,17 @@ module Main (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) (RES
         | Ok (name, key) -> (name, key) :: acc)
         [] (Key_gen.keys ())
     in
-    load_zones conduit resolver >>= fun bindings ->
-    Logs.info (fun m -> m "found %d bindings: %a" (List.length bindings)
-                  Fmt.(list ~sep:(unit ",@ ") (pair ~sep:(unit ": ") string int))
-                  (List.map (fun (k, v) -> k, String.length v) bindings)) ;
-    let trie =
-      List.fold_left (fun trie (k, data) ->
-          match Udns_zonefile.load [] data with
-          | Error msg ->
-            Logs.err (fun m -> m "error while loading zonefile %s: %s" k msg) ;
-            trie
-          | Ok rrs ->
-            let trie' = Udns_trie.insert_map (Udns_map.of_rrs rrs) trie in
-            match Udns_trie.check trie' with
-            | Ok () -> trie'
-            | Error e ->
-              Logs.err (fun m -> m "error (while processing %s) %a during check()"
-                           k Udns_trie.pp_err e) ;
-              trie)
-        Udns_trie.empty bindings
+    load_git conduit resolver >>= fun trie ->
+    let on_update t = store_zones t resolver conduit in
+    let on_notify _ t =
+      load_git conduit resolver >|= fun trie ->
+      Some trie
     in
     let t =
       Udns_server.Primary.create ~keys ~a:[Udns_server.Authentication.tsig_auth]
         ~tsig_verify:Udns_tsig.verify ~tsig_sign:Udns_tsig.sign
         ~rng:R.generate trie
     in
-    D.primary s t ;
+    D.primary ~on_update ~on_notify s t ;
     S.listen s
 end
