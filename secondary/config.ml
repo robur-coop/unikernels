78530793d5eb102b43a3d8b8ba8c8e5360f98c94
Hannes Mehnert <hannes@mehnert.org>
2019-11-10 22:28:11 +0100

    secondary: use monitoring and syslog, and as250 network interface

--- secondary/config.ml
+++ secondary/config.ml
@@ -6,19 +6,40 @@ let keys =
   let doc = Key.Arg.info ~doc:"nsupdate keys (name:type:value,...)" ["keys"] in
   Key.(create "keys" Arg.(opt (list string) [] doc))
 
+let monitor =
+  let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
+  Key.(create "monitor" Arg.(opt ipv4_address Ipaddr.V4.unspecified doc))
+
+let syslog =
+  let doc = Key.Arg.info ~doc:"syslog host IP" ["syslog"] in
+  Key.(create "syslog" Arg.(opt ipv4_address Ipaddr.V4.unspecified doc))
+
+let name =
+  let doc = Key.Arg.info ~doc:"Name of the unikernel" ["name"] in
+  Key.(create "name" Arg.(opt string "sn.nqsb.io" doc))
+
 let dns_handler =
   let packages =
     [
       package "logs" ;
       package ~min:"4.3.0" ~sublibs:["mirage"] "dns-server";
       package "dns-tsig";
+      package "monitoring-experiments";
+      package ~sublibs:["mirage"] "logs-syslog";
     ]
-  and keys = Key.([ abstract keys ])
+  and keys = [
+    Key.abstract keys ;
+    Key.abstract name ; Key.abstract monitor ; Key.abstract syslog
+  ]
   in
   foreign
+    ~deps:[abstract app_info]
     ~keys
     ~packages
-    "Unikernel.Main" (random @-> pclock @-> mclock @-> time @-> stackv4 @-> job)
+    "Unikernel.Main" (console @-> random @-> pclock @-> mclock @-> time @-> stackv4 @-> stackv4 @-> job)
+
+let management_stack = generic_stackv4 ~group:"management" (netif ~group:"management" "management")
+let service_stack = generic_stackv4 (netif ~group:"as250" "as250")
 
 let () =
-  register "secondary" [dns_handler $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ generic_stackv4 default_network ]
+  register "secondary" [dns_handler $ default_console $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ service_stack $ management_stack ]
--- secondary/unikernel.ml
+++ secondary/unikernel.ml
@@ -1,8 +1,25 @@
 (* (c) 2017, 2018 Hannes Mehnert, all rights reserved *)
-module Main (R : Mirage_random.S) (P : Mirage_clock.PCLOCK) (M : Mirage_clock.MCLOCK) (T : Mirage_time.S) (S : Mirage_stack.V4) = struct
+module Main (C : Mirage_console.S) (R : Mirage_random.S) (P : Mirage_clock.PCLOCK) (M : Mirage_clock.MCLOCK) (T : Mirage_time.S) (S : Mirage_stack.V4) (Management : Mirage_stack.V4) = struct
   module D = Dns_server_mirage.Make(P)(M)(T)(S)
 
-  let start _rng _pclock _mclock _ s =
+  module Monitoring = Monitoring_experiments.Make(T)(Management)
+  module Syslog = Logs_syslog_mirage.Udp(C)(P)(Management)
+
+  let start c _rng _pclock _mclock _ s management info =
+    let hostname = Key_gen.name ()
+    and syslog = Key_gen.syslog ()
+    and monitor = Key_gen.monitor ()
+    in
+    if Ipaddr.V4.compare syslog Ipaddr.V4.unspecified = 0 then
+      Logs.warn (fun m -> m "no syslog specified, dumping on stdout")
+    else
+      Logs.set_reporter (Syslog.create c management syslog ~hostname ());
+    if Ipaddr.V4.compare monitor Ipaddr.V4.unspecified = 0 then
+      Logs.warn (fun m -> m "no monitor specified, not outputting statistics")
+    else
+      Monitoring.create ~hostname monitor management;
+    List.iter (fun (p, v) -> Logs.app (fun m -> m "used package: %s %s" p v))
+      info.Mirage_info.packages;
     let keys = List.fold_left (fun acc str ->
         match Dns.Dnskey.name_key_of_string str with
         | Error (`Msg msg) -> Logs.err (fun m -> m "key parse error %s" msg) ; exit 64
